1b77e7f456041d9242ed3c73451abe756e95797c
Emmanuel184 <111292487+Emmanuel184@users.noreply.github.com>
2025-01-30 23:27:30 -0600

    Update page.tsx
    Changed from green to offwhite background

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       
       {/* Wrap your content inside the BackgroundBeamsWithCollision */}
-      <BackgroundBeamsWithCollision className="absolute inset-0 z-0 bg-[#E6F4E6]">
+      <BackgroundBeamsWithCollision className="absolute inset-0 z-0 bg-[#FAF9F6]">
         <div className="relative z-10 w-full flex justify-end pr-16">
           <WelcomeAnimation />
         </div>
