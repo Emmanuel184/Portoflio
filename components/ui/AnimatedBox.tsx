1e243a886fccd38a8eb8a6876bbe768d68fb0704
Emmanuel184 <111292487+Emmanuel184@users.noreply.github.com>
2025-02-01 14:10:29 -0600

    Deployment Fix

--- components/ui/AnimatedBox.tsx
+++ components/ui/AnimatedBox.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useMemo, useState } from "react";
 import { cn } from "@/lib/utils";
